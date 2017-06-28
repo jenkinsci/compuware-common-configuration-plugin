df734627d2ad0e717630dcfb62127a49ac216288
sallenCPWR <scott.allen@compuware.com>
2017-06-28 08:08:43 -0400

    CWE-114858: Update config for Global compuware common plugin (Host credentials) https://agile.compuware.com/browse/CWE-114858

--- src/main/java/com/compuware/jenkins/common/configuration/HostConnection.java
+++ src/main/java/com/compuware/jenkins/common/configuration/HostConnection.java
@@ -1,26 +1,18 @@
-/*
- * Jenkins Plugin for SonarQube, open source software quality management tool. mailto:contact AT sonarsource DOT com
- *
- * Jenkins Plugin for SonarQube is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser
- * General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any
- * later version.
- *
- * Jenkins Plugin for SonarQube is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with this program; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
-/*
- * Sonar is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
- *
- * Sonar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with Sonar; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02
+/**
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2017 Compuware Corporation
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package com.compuware.jenkins.common.configuration;
 
@@ -28,22 +20,32 @@ import java.io.Serializable;
 import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 
+/**
+ * Class used to store the data for a host connection.
+ */
 public class HostConnection implements Serializable
 {
-
 	private static final long serialVersionUID = 1L;
 
 	private final String m_connectionName;
 	private final String m_hostPort;
-	// private final String m_credentialsId;
 	private final String m_codePage;
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param connectionName
+	 *            the name of the host connection
+	 * @param hostPort
+	 *            the host:port
+	 * @param codePage
+	 *            the code page to be used for the host connection
+	 */
 	@DataBoundConstructor
-	public HostConnection(String connectionName, String hostPort, String credentialsId, String codePage)
+	public HostConnection(String connectionName, String hostPort, String codePage)
 	{
 		m_connectionName = StringUtils.trimToEmpty(connectionName);
 		m_hostPort = StringUtils.trimToEmpty(hostPort);
-		// m_credentialsId = StringUtils.trimToEmpty(credentialsId);
 		m_codePage = StringUtils.trimToEmpty(codePage);
 	}
 
@@ -61,16 +63,6 @@ public class HostConnection implements Serializable
 	{
 		return m_hostPort;
 	}
-	
-	/**
-	 * Gets the value of the 'Login Credentials'.
-	 * 
-	 * @return <code>String</code> value of m_credentialsId
-	 */
-	// public String getCredentialsId()
-	// {
-	// return m_credentialsId;
-	// }
 	
 	/**
 	 * Gets the value of the 'Code Page'.
