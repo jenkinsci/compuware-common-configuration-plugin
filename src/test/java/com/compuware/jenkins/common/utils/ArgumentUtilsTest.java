4fa9ded8b228b037d596d6aeaa7cb83e7cc24e24
Derek Rea <derek.rea@compuware.com>
2017-07-31 17:05:54 -0400

    Added/updated unit tests.

--- src/test/java/com/compuware/jenkins/common/utils/ArgumentUtilsTest.java
+++ src/test/java/com/compuware/jenkins/common/utils/ArgumentUtilsTest.java
@@ -30,21 +30,84 @@ import org.junit.Test;
 public class ArgumentUtilsTest
 {
 	/**
-	 * Test method for {@link com.compuware.jenkins.common.utils.ArgumentUtils#escapeForScript(java.lang.String, boolean)}.
+	 * Test method for {@link com.compuware.jenkins.common.utils.ArgumentUtils#escapeForScript(java.lang.String)}.
 	 */
 	@Test
 	public void testEscapeForScript()
 	{
-		// TODO fail("Not yet implemented");
+		String strWithDoubleQuotesAndWrapped = "\"double\"Quotes\"Wrapped\"Test\"";
+		String strWithoutDoubleQuotesAndWrapped = "\"doubleQuotesWrappedTest\"";
+		String strWithDoubleQuotesNotWrapped = "double\"Quotes\"Wrapped\"Test";
+		String strWithoutDoubleQuotesNotWrapped = "doubleQuotesWrappedTest";
+
+		String expectedStr = "\"\"double\"\"Quotes\"\"Wrapped\"\"Test\"\"";
+		String strWithDoubleQuotesAndWrappedAfterCall = ArgumentUtils.escapeForScript(strWithDoubleQuotesAndWrapped);
+		assertThat("Expected the string with double quotes and wrapped to be changed after call.",
+				strWithDoubleQuotesAndWrappedAfterCall, equalTo(expectedStr));
+
+		expectedStr = "\"\"doubleQuotesWrappedTest\"\"";
+		String strWithoutDoubleQuotesAndWrappedAfterCall = ArgumentUtils.escapeForScript(strWithoutDoubleQuotesAndWrapped);
+		assertThat("Expected the string without double quotes and wrapped to not be changed after call.",
+				strWithoutDoubleQuotesAndWrappedAfterCall, equalTo(expectedStr));
+
+		expectedStr = "\"double\"\"Quotes\"\"Wrapped\"\"Test\"";
+		String strWithDoubleQuotesNotWrappedAfterCall = ArgumentUtils.escapeForScript(strWithDoubleQuotesNotWrapped);
+		assertThat("Expected the string with double quotes not wrapped to be changed after call.",
+				strWithDoubleQuotesNotWrappedAfterCall, equalTo(expectedStr));
+
+		String strWithoutDoubleQuotesNotWrappedAfterCall = ArgumentUtils.escapeForScript(strWithoutDoubleQuotesNotWrapped);
+		assertThat("Expected the string without double quotes not wrapped to be changed after call.",
+				strWithoutDoubleQuotesNotWrappedAfterCall, equalTo(strWithoutDoubleQuotesAndWrapped));
 	}
 
 	/**
-	 * Test method for {@link com.compuware.jenkins.common.utils.ArgumentUtils#wrapInQuotes(java.lang.String)}.
+	 * Test method for {@link com.compuware.jenkins.common.utils.ArgumentUtils#escapeCommaDelimitedPathsForScript(String)}.
 	 */
 	@Test
-	public void testWrapInQuotes()
+	public void testEscapeCommaDelimitedPathsForScript()
 	{
-		// TODO fail("Not yet implemented");
+		String strWithoutDoubleQuotesAndWrapped = "\"doubleQuotesWrappedTest\"";
+		String strWithDoubleQuotesAndWrapped = "\"double\"Quotes\"Wrapped\"Test\"";
+		String strWithoutDoubleQuotesNotWrapped = "doubleQuotesWrappedTest";
+		String strWithDoubleQuotesNotWrapped = "double\"Quotes\"Wrapped\"Test";
+
+		String strWithoutDoubleQuotesAndWrappedAfterCall = ArgumentUtils
+				.escapeCommaDelimitedPathsForScript(strWithoutDoubleQuotesAndWrapped);
+		assertThat("Expected the string without double quotes and wrapped to not be changed after call.",
+				strWithoutDoubleQuotesAndWrappedAfterCall, equalTo(strWithoutDoubleQuotesAndWrapped));
+
+		String strWithDoubleQuotesAndWrappedAfterCall = ArgumentUtils
+				.escapeCommaDelimitedPathsForScript(strWithDoubleQuotesAndWrapped);
+		assertThat("Expected the string with double quotes and wrapped to be changed after call.",
+				strWithDoubleQuotesAndWrappedAfterCall, equalTo(strWithoutDoubleQuotesAndWrapped));
+
+		String strWithoutDoubleQuotesNotWrappedAfterCall = ArgumentUtils
+				.escapeCommaDelimitedPathsForScript(strWithoutDoubleQuotesNotWrapped);
+		assertThat("Expected the string without double quotes not wrapped to be changed after call.",
+				strWithoutDoubleQuotesNotWrappedAfterCall, equalTo(strWithoutDoubleQuotesAndWrapped));
+
+		String strWithDoubleQuotesNotWrappedAfterCall = ArgumentUtils
+				.escapeCommaDelimitedPathsForScript(strWithDoubleQuotesNotWrapped);
+		assertThat("Expected the string with double quotes not wrapped to be changed after call.",
+				strWithDoubleQuotesNotWrappedAfterCall, equalTo(strWithoutDoubleQuotesAndWrapped));
+	}
+
+	/**
+	 * Test method for {@link com.compuware.jenkins.common.utils.ArgumentUtils#wrapInDoubleQuotes(java.lang.String)}.
+	 */
+	@Test
+	public void testWrapInDoubleQuotes()
+	{
+		String strWithDoubleQuotes = "\"doubleQuotesTest\"";
+		String strWithoutDoubleQuotes = "doubleQuotesTest";
+
+		String strWithDoubleQuotesAfterCall = ArgumentUtils.wrapInDoubleQuotes(strWithDoubleQuotes);
+		assertThat("Expected the string with double quotes to not be changed after call.", strWithDoubleQuotesAfterCall,
+				equalTo(strWithDoubleQuotes));
+
+		String strWithoutDoubleQuotesAfterCall = ArgumentUtils.wrapInDoubleQuotes(strWithoutDoubleQuotes);
+		assertThat("Expected the string without double quotes to be changed after call.", strWithoutDoubleQuotesAfterCall,
+				equalTo(strWithDoubleQuotes));
 	}
 
 	/**
@@ -53,8 +116,8 @@ public class ArgumentUtilsTest
 	@Test
 	public void testPrefixWithDash()
 	{
-		String strWithDash = "-hasDash";
-		String strWithoutDash = "hasDash";
+		String strWithDash = "-dashTest";
+		String strWithoutDash = "dashTest";
 
 		String strWithDashAfterCall = ArgumentUtils.prefixWithDash(strWithDash);
 		assertThat("Expected the string with a dash to not be changed after call.", strWithDashAfterCall, equalTo(strWithDash));
@@ -70,56 +133,56 @@ public class ArgumentUtilsTest
 	@Test
 	public void testConvertStringToProperties() throws IOException
 	{
-		Properties expectedPropertiesWithoutDashes = new Properties();
-		expectedPropertiesWithoutDashes.put("cc.sources", "/testSrc,C:/Users/");
-		expectedPropertiesWithoutDashes.setProperty("cc.repos", "XDEVREG.CC.REPOSIT,XDEVREG.CC.REPOSIT2");
-		expectedPropertiesWithoutDashes.setProperty("cc.system", "ccSystem");
-		expectedPropertiesWithoutDashes.setProperty("cc.test", "ccTest");
-		expectedPropertiesWithoutDashes.setProperty("cc.ddio.overrides", "XDEVREG.CC.DDIO,XDEVREG.CC.DDIO2");
+		Properties expectedPropertiesWithoutDashesOrBackslashes = new Properties();
+		expectedPropertiesWithoutDashesOrBackslashes.put("cc.sources", "/testSrc,C:/Users/");
+		expectedPropertiesWithoutDashesOrBackslashes.setProperty("cc.repos", "XDEVREG.CC.REPOSIT,XDEVREG.CC.REPOSIT2");
+		expectedPropertiesWithoutDashesOrBackslashes.setProperty("cc.system", "ccSystem");
+		expectedPropertiesWithoutDashesOrBackslashes.setProperty("cc.test", "ccTest");
+		expectedPropertiesWithoutDashesOrBackslashes.setProperty("cc.ddio.overrides", "XDEVREG.CC.DDIO,XDEVREG.CC.DDIO2");
 
-		Properties expectedPropertiesWithDashes = new Properties();
-		expectedPropertiesWithDashes.put("-cc.sources", "/testSrc,C:/Users/");
-		expectedPropertiesWithDashes.setProperty("-cc.repos", "XDEVREG.CC.REPOSIT,XDEVREG.CC.REPOSIT2");
-		expectedPropertiesWithDashes.setProperty("-cc.system", "ccSystem");
-		expectedPropertiesWithDashes.setProperty("-cc.test", "ccTest");
-		expectedPropertiesWithDashes.setProperty("-cc.ddio.overrides", "XDEVREG.CC.DDIO,XDEVREG.CC.DDIO2");
+		Properties expectedPropertiesWithDashesAndBackslashes = new Properties();
+		expectedPropertiesWithDashesAndBackslashes.put("-cc.sources", "\\testSrc,C:\\Users\\");
+		expectedPropertiesWithDashesAndBackslashes.setProperty("-cc.repos", "XDEVREG.CC.REPOSIT,XDEVREG.CC.REPOSIT2");
+		expectedPropertiesWithDashesAndBackslashes.setProperty("-cc.system", "ccSystem");
+		expectedPropertiesWithDashesAndBackslashes.setProperty("-cc.test", "ccTest");
+		expectedPropertiesWithDashesAndBackslashes.setProperty("-cc.ddio.overrides", "XDEVREG.CC.DDIO,XDEVREG.CC.DDIO2");
 
-		// test property String without dashes, without whitespace, with newlines
+		// test property String without dashes, with newlines
 		String propertyStr1 = "cc.sources=/testSrc,C:/Users/\ncc.repos=XDEVREG.CC.REPOSIT,XDEVREG.CC.REPOSIT2\ncc.system=ccSystem\ncc.test=ccTest\ncc.ddio.overrides=XDEVREG.CC.DDIO,XDEVREG.CC.DDIO2";
 		Properties properties1 = ArgumentUtils.convertStringToProperties(propertyStr1);
 		assertThat("Expected property String without dashes, with newlines to be converted to Properties correctly.",
-				properties1.entrySet(), is(expectedPropertiesWithoutDashes.entrySet()));
+				properties1.entrySet(), is(expectedPropertiesWithoutDashesOrBackslashes.entrySet()));
 
 		// test property String with dashes, with newlines
-		String propertyStr2 = "-cc.sources=/testSrc,C:/Users/\n-cc.repos=XDEVREG.CC.REPOSIT,XDEVREG.CC.REPOSIT2\n-cc.system=ccSystem\n-cc.test=ccTest\n-cc.ddio.overrides=XDEVREG.CC.DDIO,XDEVREG.CC.DDIO2";
+		String propertyStr2 = "-cc.sources=\\testSrc,C:\\Users\\\n-cc.repos=XDEVREG.CC.REPOSIT,XDEVREG.CC.REPOSIT2\n-cc.system=ccSystem\n-cc.test=ccTest\n-cc.ddio.overrides=XDEVREG.CC.DDIO,XDEVREG.CC.DDIO2";
 		Properties properties2 = ArgumentUtils.convertStringToProperties(propertyStr2);
 		assertThat("Expected property String with dashes, with newlines to be converted to Properties correctly.",
-				properties2.entrySet(), is(expectedPropertiesWithDashes.entrySet()));
+				properties2.entrySet(), is(expectedPropertiesWithDashesAndBackslashes.entrySet()));
 
 		// test property String without dashes, with carriage returns
 		String propertyStr3 = "cc.sources=/testSrc,C:/Users/\rcc.repos=XDEVREG.CC.REPOSIT,XDEVREG.CC.REPOSIT2\rcc.system=ccSystem\rcc.test=ccTest\rcc.ddio.overrides=XDEVREG.CC.DDIO,XDEVREG.CC.DDIO2";
 		Properties properties3 = ArgumentUtils.convertStringToProperties(propertyStr3);
 		assertThat("Expected property String without dashes, with carraige returns to be converted to Properties correctly.",
-				properties3.entrySet(), is(expectedPropertiesWithoutDashes.entrySet()));
+				properties3.entrySet(), is(expectedPropertiesWithoutDashesOrBackslashes.entrySet()));
 
 		// test property String with dashes, with carriage returns
-		String propertyStr4 = "-cc.sources=/testSrc,C:/Users/\r-cc.repos=XDEVREG.CC.REPOSIT,XDEVREG.CC.REPOSIT2\r-cc.system=ccSystem\r-cc.test=ccTest\r-cc.ddio.overrides=XDEVREG.CC.DDIO,XDEVREG.CC.DDIO2";
+		String propertyStr4 = "-cc.sources=\\testSrc,C:\\Users\\\r-cc.repos=XDEVREG.CC.REPOSIT,XDEVREG.CC.REPOSIT2\r-cc.system=ccSystem\r-cc.test=ccTest\r-cc.ddio.overrides=XDEVREG.CC.DDIO,XDEVREG.CC.DDIO2";
 		Properties properties4 = ArgumentUtils.convertStringToProperties(propertyStr4);
 		assertThat("Expected property String with dashes, with carraige returns to be converted to Properties correctly.",
-				properties4.entrySet(), is(expectedPropertiesWithDashes.entrySet()));
+				properties4.entrySet(), is(expectedPropertiesWithDashesAndBackslashes.entrySet()));
 
 		// test property String without dashes, with newline/carriage returns
 		String propertyStr5 = "cc.sources=/testSrc,C:/Users/\n\rcc.repos=XDEVREG.CC.REPOSIT,XDEVREG.CC.REPOSIT2\n\rcc.system=ccSystem\n\rcc.test=ccTest\n\rcc.ddio.overrides=XDEVREG.CC.DDIO,XDEVREG.CC.DDIO2";
 		Properties properties5 = ArgumentUtils.convertStringToProperties(propertyStr5);
 		assertThat(
 				"Expected property String without dashes, with newline/carraige returns to be converted to Properties correctly.",
-				properties5.entrySet(), is(expectedPropertiesWithoutDashes.entrySet()));
+				properties5.entrySet(), is(expectedPropertiesWithoutDashesOrBackslashes.entrySet()));
 
 		// test property String with dashes, with newline/carriage returns
-		String propertyStr6 = "-cc.sources=/testSrc,C:/Users/\n\r-cc.repos=XDEVREG.CC.REPOSIT,XDEVREG.CC.REPOSIT2\n\r-cc.system=ccSystem\n\r-cc.test=ccTest\n\r-cc.ddio.overrides=XDEVREG.CC.DDIO,XDEVREG.CC.DDIO2";
+		String propertyStr6 = "-cc.sources=\\testSrc,C:\\Users\\\n\r-cc.repos=XDEVREG.CC.REPOSIT,XDEVREG.CC.REPOSIT2\n\r-cc.system=ccSystem\n\r-cc.test=ccTest\n\r-cc.ddio.overrides=XDEVREG.CC.DDIO,XDEVREG.CC.DDIO2";
 		Properties properties6 = ArgumentUtils.convertStringToProperties(propertyStr6);
 		assertThat(
 				"Expected property String with dashes, with newline/carraige returns to be converted to Properties correctly.",
-				properties6.entrySet(), is(expectedPropertiesWithDashes.entrySet()));
+				properties6.entrySet(), is(expectedPropertiesWithDashesAndBackslashes.entrySet()));
 	}
 }
