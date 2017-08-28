5a6493b14cbd7c263708f4752b5067ac52c03b9a
sallenCPWR <scott.allen@compuware.com>
2017-08-28 11:11:38 -0400

    CWE-117020: Jenkins Plugins / CLI capability check https://agile.compuware.com/browse/CWE-117020 updated the compareVersions method

--- src/main/java/com/compuware/jenkins/common/utils/CLIVersionUtils.java
+++ src/main/java/com/compuware/jenkins/common/utils/CLIVersionUtils.java
@@ -24,6 +24,7 @@ import java.io.InputStream;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import org.apache.commons.lang.StringUtils;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 import com.compuware.jenkins.common.configuration.Messages;
@@ -64,9 +65,13 @@ public class CLIVersionUtils
 		
 		String version = getCLIVersion(cliDirectory, minimumVersion);
 		
-		if (compareVersions(version, minimumVersion) != 0)
+		if (StringUtils.isEmpty(version))
 		{
-			throw new AbortException(Messages.cliOldVersionError(minimumVersion));
+			throw new AbortException(Messages.cliOldUnknownVersionError(minimumVersion));
+		}
+		else if (compareVersions(version, minimumVersion) < 0)
+		{
+			throw new AbortException(Messages.cliOldVersionError(version, minimumVersion));
 		}
 	}
 	
@@ -81,7 +86,7 @@ public class CLIVersionUtils
 		
 		if (!versionFilePath.exists())
 		{
-			throw new AbortException(Messages.cliOldVersionError(minimumVersion));
+			throw new AbortException(Messages.cliOldUnknownVersionError(minimumVersion));
 		}
 				
 		version = parseXml(versionFilePath.read());
@@ -94,15 +99,31 @@ public class CLIVersionUtils
 	 */
 	private static int compareVersions(String cliVersion, String minimumVersion)
 	{
-		if (cliVersion.equalsIgnoreCase("")) //$NON-NLS-1$
+		if (StringUtils.isEmpty(cliVersion))
 		{
-			return 1;
+			return -1;
 		}
-		
-		int cliVersionNum = Integer.parseInt(cliVersion.replaceAll("\\.", "")); //$NON-NLS-1$//$NON-NLS-2$
-		int minimumVersionNum = Integer.parseInt(minimumVersion.replaceAll("\\.", "")); //$NON-NLS-1$//$NON-NLS-2$
-		
-		return (cliVersionNum < minimumVersionNum) ? 1 : 0;
+
+		String[] minimumVersionParts = StringUtils.split(minimumVersion, '.');
+		String[] cliVersionParts = StringUtils.split(cliVersion, '.');
+
+		int length = Math.max(minimumVersionParts.length, cliVersionParts.length);
+		for (int i = 0; i < length; i++)
+		{
+			int minimumVersionPart = i < minimumVersionParts.length ? Integer.parseInt(minimumVersionParts[i]) : 0;
+			int cliVersionPart = i < cliVersionParts.length ? Integer.parseInt(cliVersionParts[i]) : 0;
+
+			if (cliVersionPart < minimumVersionPart)
+			{
+				return -1;
+			}
+			else if (cliVersionPart > minimumVersionPart)
+			{
+				return 1;
+			}
+		}
+
+		return 0;
 	}
 	
 	/*
